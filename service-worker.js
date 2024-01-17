b1ea5ef787837682d231ecbe8f9df02ab60e120e
unknown <michael.stillwell@hotmail.com>
2024-01-16 20:45:35 -0600

    feat: pwa offline

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'my-pwa-cache';
+const urlsToCache = [
+  './',
+  'index.html',
+  'manifest.json',
+  // Add any other static assets you want to cache
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
